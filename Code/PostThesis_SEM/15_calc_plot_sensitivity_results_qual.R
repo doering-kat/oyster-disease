cb195b8b6dddd66549085d73113a08784944a138
doering-kat <doering.kat@gmail.com>
2021-04-04 14:03:40 -0700

    refine qualitative sensitivity plot
    uses patchwork package

--- Code/PostThesis_SEM/15_calc_plot_sensitivity_results_qual.R
+++ Code/PostThesis_SEM/15_calc_plot_sensitivity_results_qual.R
@@ -9,6 +9,7 @@ library(tidyr)
 library(dplyr)
 library(DescTools)
 library(ggplot2)
+library(patchwork)
 options(stringsAsFactors = FALSE)
 
 # Load data ----
@@ -104,19 +105,78 @@ scen_num_key <- select(scen_paste, scen_number, Scenario, scenario.type)
 change_df_all <- left_join(change_df_all, scen_num_key, by = "scen_number")
 change_df_all$Scenario_fac <- factor(change_df_all$scen_number, levels = scen_num_key$scen_number, labels = scen_num_key$Scenario )
 
+change_df_all$Scenario_fac_order <- factor(change_df_all$Scenario, 
+ levels = c( "temp_4-10; sal_4-10", #base scenario
+            "temp_1-2; sal_3-8", "temp_1-2; sal_4-8", "temp_1-2; sal_5-8", "temp_1-2; sal_6-8", "temp_1-2; sal_7-8",
+            "temp_1-2; sal_3-9", "temp_1-2; sal_4-9", "temp_1-2; sal_5-9", "temp_1-2; sal_6-9", "temp_1-2; sal_7-9",
+            "temp_1-2; sal_3-10", "temp_1-2; sal_4-10", "temp_1-2; sal_5-10", "temp_1-2; sal_6-10", "temp_1-2; sal_7-10", 
+            # summer temp scens 
+            "temp_3-8; sal_3-8", "temp_4-8; sal_4-8", "temp_5-8; sal_5-8", "temp_6-8; sal_6-8", "temp_7-8; sal_7-8",
+            "temp_3-9; sal_3-9", "temp_4-9; sal_4-9", "temp_5-9; sal_5-9", "temp_6-9; sal_6-9", "temp_7-9; sal_7-9",
+            "temp_3-10; sal_3-10",                  "temp_5-10; sal_5-10", "temp_6-10; sal_6-10", "temp_7-10; sal_7-10"
+             ), 
+ labels = c( "base", #base scenario
+             "sal_3-8", "sal_4-8", "sal_5-8", "sal_6-8", "sal_7-8",
+             "sal_3-9", "sal_4-9", "sal_5-9", "sal_6-9", "sal_7-9",
+             "sal_3-10", "sal_4-10", "sal_5-10", "sal_6-10", "sal_7-10", 
+             
+             "temp 3-8; sal 3-8", "temp 4-8; sal 4-8", "temp 5-8; sal 5-8", "temp 6-8; sal 6-8", "temp 7-8; sal 7-8",
+             "temp 3-9; sal 3-9", "temp 4-9; sal 4-9", "temp 5-9; sal 5-9", "temp 6-9; sal 6-9", "temp 7-9; sal 7-9",
+             "temp 3-10; sal 3-10",                  "temp 5-10; sal 5-10", "temp 6-10; sal 6-10", "temp 7-10; sal 7-10"
+             ))
+
+change_df_all$sal_zone_fac <- factor(change_df_all$sal_zone, levels = c("Low", "MedHigh"), labels = c("Low", "Med./High"))
+change_df_all$relationship_fac <-factor(change_df_all$relationship,
+  levels =  c("MSX_Prev_Per~wtemp_cen", "MSX_Prev_Per~sal_cen", "M_Med_A_Bar_Per~MSX_Prev_Per",
+              "Dermo_Prev_Per~wtemp_cen", "Dermo_Prev_Per~sal_cen", "M_Med_A_Bar_Per~Dermo_Prev_Per"), 
+  labels = c("Temperature -> % MSX Prevalence", "Salinity -> % MSX Prevalence", "MSX % Prevalence -> % Mortality", 
+             "Temperature -> % Dermo Prevalence", "Salinity -> % Dermo Prevalence", "Dermo % Prevalence -> % Mortality"))
+
+change_df_all[change_df_all$Scenario == "temp_4-10; sal_4-10","scenario.type"] <- "base"
 # save qualitative change data ----
 # TODO
 
 # plot and save qualitative change ----
 # to do: figure out how to plot; 
 
-ggplot2::ggplot(data = change_df_all, aes(x = Scenario_fac, y = sal_zone)) +
+ggplot2::ggplot(data = change_df_all, aes(x = Scenario_fac_order, y = sal_zone_fac)) +
   geom_tile(aes(fill = change), color = "black") +
-  facet_wrap(.~relationship)+
+  facet_wrap(.~relationship_fac)+
   theme_classic(base_size = 12)+
+  xlab("Scenario")+
+  ylab("Salinity Zone")+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 ggsave(file.path(fig_save_path, "qual_diff_tile_plot.png"), height = 8,
   width = 12, units = "in")
 
 
+# split winter scenarios
 
+winter_plot <- ggplot2::ggplot(data = subset(change_df_all, scenario.type %in% c("base", "winter_temp")), aes(x = Scenario_fac_order, y = sal_zone_fac)) +
+  geom_tile(aes(fill = change), color = "black") +
+  facet_wrap(.~relationship_fac)+
+  theme_classic(base_size = 14)+
+  xlab("Winter Temp Scenario")+
+  ylab("Salinity Zone")+
+  labs(fill = "Change over time" ) +
+  scale_fill_manual(values = c("#f0f0f0", "#636363"), na.value = "#bdbdbd")+
+  theme(legend.position = "top") +
+  guides(fill = guide_legend(title.position = "top"))+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+summer_plot <- ggplot2::ggplot(data = subset(change_df_all, scenario.type %in% c("base", "summer_temp_sal")), aes(x = Scenario_fac_order, y = sal_zone_fac)) +
+  geom_tile(aes(fill = change), color = "black") +
+  facet_wrap(.~relationship_fac)+
+  theme_classic(base_size = 14)+
+  xlab("Summer Temp Scenario")+
+  ylab("Salinity Zone")+
+  scale_fill_manual(values = c("#f0f0f0", "#636363"), na.value = "#bdbdbd")+
+  theme(legend.position = "none") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+
+winter_plot / summer_plot +
+  plot_annotation(tag_levels = "A")
+
+ggsave(file.path(fig_save_path, "qual_sens_plot_publication.png"), width = 12, height = 8, 
+       units = "in", dpi = "print" )
